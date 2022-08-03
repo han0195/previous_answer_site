0110862596a70e07977afb87b55a3ff6f1c4f62c
han0195 <gks0195@naver.com>
2022-08-03 10:50:10 +0900

    전체풀기

--- src/main/resources/static/js/allexam.js
+++ src/main/resources/static/js/allexam.js
@@ -37,7 +37,7 @@ function inserthtml(){
         for(let j = 0 ; j < 2 ; j++){ /* 두개씩 출력 */
             if(testdata[count] != null){
                 html += ' <div class="col-sm-6">';
-                html += '<div class="titlediv">'+testdata[count].pindex+". "+testdata[count].pname+'</div>';
+                html += '<div class="titlediv">'+testdata[count].pindex+". "+testdata[count].pname+' <a onclick="resetchoice('+testdata[count].pno+')">선택초기화</a></div>';
                 if(testdata[count].pimg.length == 0){ /* 사진이 존재하지않는다면 */
                     html += '<div></div>'
                 }else {
@@ -46,9 +46,9 @@ function inserthtml(){
                 html += '<ul class="chul">';
                 for(let z = 0; z < testdata[count].poption.length ; z++){ /* 문제보기 반복 */
                     if(testdata[count].panswer.length > 1){ /* 중복 답*/
-                        html += '<li><span onclick="choice('+(z + 1)+', '+testdata[count].pno+', '+true+')">'+indexproblem[z]+'</span>'+testdata[count].poption[z]+'</li>';
+                        html += '<li><span id="problem'+z+'_'+testdata[count].pno+'" onclick="choice('+(z + 1)+', '+testdata[count].pno+', '+true+')">'+indexproblem[z]+'</span>'+testdata[count].poption[z]+'</li>';
                     }else{ /* 단일 답 */
-                        html += '<li><span onclick="choice('+(z + 1)+', '+testdata[count].pno+', '+false+')">'+indexproblem[z]+'</span>'+testdata[count].poption[z]+'</li>';
+                        html += '<li><span id="problem'+z+'_'+testdata[count].pno+'" onclick="choice('+(z + 1)+', '+testdata[count].pno+', '+false+')">'+indexproblem[z]+'</span>'+testdata[count].poption[z]+'</li>';
                     }
                 }
                 html += '</ul>';
@@ -95,4 +95,46 @@ function choice(choicenum, pno, duplicate_selection){
         }
     }
 
+    choicecoloc(choicenum, pno, duplicate_selection);
+
 }
+
+function  choicecoloc(choicenum, pno, duplicate_selection){
+    if(duplicate_selection){ /* 중복 문제 */
+        //선택번호 색깔 변경
+        $('#problem'+(choicenum-1)+'_'+pno+'').html('●');
+    }else {
+        //초기화
+        $('#problem0_'+pno+'').html('①');
+        $('#problem1_'+pno+'').html('②');
+        $('#problem2_'+pno+'').html('③');
+        $('#problem3_'+pno+'').html('④');
+
+        //선택번호 색깔 변경
+        $('#problem'+(choicenum-1)+'_'+pno+'').html('●');
+    }
+    
+}
+
+function resetchoice(pno){
+    //html 초기화
+    $('#problem0_'+pno+'').html('①');
+    $('#problem1_'+pno+'').html('②');
+    $('#problem2_'+pno+'').html('③');
+    $('#problem3_'+pno+'').html('④');
+    
+    //해당 선택 객체 삭제
+
+    for(let i = 0 ; i < user_choice_anwser.length ; i++){
+        if(user_choice_anwser[i].pno == pno){
+            user_choice_anwser.splice(i,1);
+        }
+    }
+
+
+}
+
+//채점 함수
+
+
+
